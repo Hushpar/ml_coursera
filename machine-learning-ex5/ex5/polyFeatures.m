ab13f605b527e3b3d66a20831d9058f21372a262
Hushpar <hushpar@icloud.com>
2015-10-04 20:17:23 +0300

    Add Polynomial features

--- machine-learning-ex5/ex5/polyFeatures.m
+++ machine-learning-ex5/ex5/polyFeatures.m
@@ -7,16 +7,17 @@ function [X_poly] = polyFeatures(X, p)
 
 
 % You need to return the following variables correctly.
-X_poly = zeros(numel(X), p);
+X_poly = ones(1, p);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Given a vector X, return a matrix X_poly where the p-th 
 %               column of X contains the values of X to the p-th power.
 %
 % 
-
-
-
+X_poly = X * X_poly;
+for i=2:p
+    X_poly(:,i) = X.^i;
+end
 
 
 
